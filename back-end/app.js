f31d46a12e2cafba8c962c5ed293687ac99a0523
sap688 <sap688@nyu.edu>
2023-11-13 17:43:31 -0500

    backend start

--- back-end/app.js
+++ back-end/app.js
@@ -2,7 +2,28 @@
 const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
 
+
+app.use(express.json()) // decode JSON-formatted incoming POST data
+app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
+
+
 // we will put some server logic here later...
+app.get("/", (req, res) => {
+    res.send("HELLO!");
+});
+
+app.get("/messages", (req, res) => {
+    // res.send("messages!");
+    res.json({
+        message1: "Hey",
+        message2: "want to play bball?",
+        message3: "sure what time works?",
+        message4: "I get off work at 5",   
+        message5: "my friend wants to join... can you find another player for a 2 on 2?",           
+    });
+});
+
+
 
 // export the express app we created to make it available to other modules
 module.exports = app
--- front-end/src/Messages.js
+++ front-end/src/Messages.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "./Messages.css"
 import { Link } from "react-router-dom";
-
+import axios from "axios"
 
 function Messages(){
     return (
