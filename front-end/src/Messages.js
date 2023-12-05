d06d5de0e5af0035a0b2046b2749951a9bab2973
sap688 <sap688@nyu.edu>
2023-12-05 12:10:34 -0500

    update merge

--- front-end/src/Messages.js
+++ front-end/src/Messages.js
@@ -1,66 +1,76 @@
-import React, { useState, useEffect } from "react"
+import React, { useContext, useState, useEffect, Pressable } from "react"
 import "./Messages.css"
 import { Link } from "react-router-dom";
 import axios from "axios"
 // import { response } from "../../back-end/app";
 import { LuMessageSquarePlus } from "react-icons/lu";
+import { UserType } from "./UserContext";
 
 
 
 const Messages = (props) => {
 
+    const [friends, setFriends] = useState([]);
+    const { userId, setUserId } = useContext(UserType);
     const [message, setMessage] = useState([])
     useEffect(() => {
-        const fetchMessage = () => {
-            axios
-                .get("http://localhost:3000/messages")
-                .then(response => {
-                    const message = response.data
-                    setMessage(message)
-        });
-        // console.log("working!!")
+        const friendsList = async () => {
+            try {
+              const response = await fetch(
+                // `http://localhost:3000/messages/${userId}` 
+                //^CHANGE?!?!
+                `http://localhost:3000/friends/${userId}` 
+
+              );
+              const data = await response.json();
+      
+              if (response.ok) {
+                setFriends(data);
+              }
+            } catch (error) {
+              console.log("error showing the friends", error);
+            }
+          };
+          friendsList();
+        }, []);
+        //   const fetchMessage = () => {
+        //     axios
+        //         .get("http://localhost:3000/messages")
+        //         .then(response => {
+        //             const message = response.data
+        //             setMessage(message)
+        // });
        
-    }
+    // }
 
         // console.log(Message)
         // return () => Message
-        fetchMessage();
-    }, [])
+        // fetchMessage();
+
+
+
+      console.log("friends",friends)
 
     return (
         <div>
             <div className="header">
                 <h2>Messages </h2>
             </div>
-            
-            {/* <Link to="/chat">
-                <h3>New Message </h3>
-            </Link> */}
-
-            {/* const Message = (props) => { */}
-
             <Link to="/chat">
-
-            {/* <div className="Message">
-                <img className="profpic" alt="welcome!" src="https://picsum.photos/200?page=home" />
-                <div className="messagetext">
-                    <h3>THIS IS THE SINGLE ONE</h3>
-                    <p>{message.from}</p>
-                    <p>{message.text}</p>
-                </div>
-            </div> */}
+                (friends.map((item, index)) => (
+                    {/* <Message key={index} item={item}/> */}
+                {/* ))} */}
+                {/* <Image source = ({uri:item?.image})/> */}
             </Link>
-
             <NewMessage />
+            <div className="Message">
+                {/* Friends.map((item,index) => ( */}
 
-
-            {/* <Message message = {message}/> */}
-
+                {/* )) */}
+            </div>
             <Message message={message}/>
             <Message message={message}/>
-            {/* <Message />
-            <Message />
-            <Message /> */}
+
         </div>
 
     );
